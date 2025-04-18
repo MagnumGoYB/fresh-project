556144719a88857ded6e6b5c9e4e654990dcbfd1
MagnumGOYB <sosbs@outlook.com>
2025-04-18 13:24:41 +0800

    🚧 chore: update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import { type Config } from 'tailwindcss'
+
+import typography from '@tailwindcss/typography'
+
 import animate from 'tailwindcss-animate'
 
 export default {
   content: ['{routes,islands,components}/**/*.{ts,tsx,js,jsx}'],
-  plugins: [animate],
+  plugins: [animate, typography],
 } satisfies Config
