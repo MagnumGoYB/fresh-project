b831887af18e8fb21a67e4101b19f7d893727ab8
MagnumGOYB <sosbs@outlook.com>
2025-04-18 11:58:28 +0800

    🚧 chore: update dev spaces

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,7 @@
 import { type Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
-import typography from '@tailwindcss/typography'
 
 export default {
-  darkMode: ['class'],
   content: ['{routes,islands,components}/**/*.{ts,tsx,js,jsx}'],
-  plugins: [animate, typography],
+  plugins: [animate],
 } satisfies Config
